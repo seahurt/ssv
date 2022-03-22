06464f61d9d0506ad7e3ecb9678e1b729be4321a
William Wang <wangjianghao@matridx.top>
2022-03-22 17:22:51 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ssv
 Single Server Virtualization with cloud image on Linux based on KVM
+
+## Minimal Feature
+- create vm on web page
+- set ip and avoid ip conflict
+- set disk/mem/cpu
+- set user password or ssh key
+- upload image or download from remote
+- init kvm and bridge network by script
+
+## TODO
+- [ ] create model
+- [ ] create api 
+- [ ] create async task to handle vm
+- [ ] create frontend page
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+import psutil
+
 
 # Create your models here.
+def get_total_memory():
+    return psutil.virtual_memory().total
+
+
+class SystemConfig(models.Model):
+    bridge_name = models.CharField(max_length=128, default='br0')
+    bridge_network = models.CharField(max_length=128, default="192.168.100.1/24")
+    dns = models.CharField(max_length=128, default='114.114.114.114,223.5.5.5')
+
+    # location
+    vm_location = models.CharField(max_length=256, default='/var/lib/ssv/vms')
+    image_location = models.CharField(max_length=256, default='/var/lib/ssv/images')
+
+    disk_size = models.IntegerField(default=100)  # unit GB
+    disk_size_upper_limit = models.IntegerField(default=1024)  # up to 1T
+
+    mem_size = models.IntegerField(default=8)
+    vcpu_count = models.IntegerField(default=4)
+
+    username = models.CharField(max_length=128, null=True, blank=True)
+    ssh_key = models.TextField(null=True, blank=True)
+    password = models.TextField(null=True, blank=True)  # store password hash here
+
+
+class BaseImage(models.Model):
+    """
+    config:
+    {
+        "os": "CentOS7",
+    }
+    """
+    name = models.CharField(max_length=128, unique=True)
+    path = models.CharField(max_length=256)
+    config = models.JSONField(default=dict, null=True, blank=True)
+
+
+class IPPool(models.Model):
+    network = models.IPAddressField()
+    gateway = models.IPAddressField()
+    netmask = models.IPAddressField()
+    known_ips = models.JSONField()
+
+
+class VM(models.Model):
+    ST_PENDING = 'pending'
+    ST_RUNNING = 'running'
+    ST_SHUTDOWN = 'shutdown'
+
+    st_choices = (
+        (ST_PENDING, ST_PENDING),
+        (ST_RUNNING, ST_RUNNING),
+        (ST_SHUTDOWN, ST_SHUTDOWN),
+    )
+    name = models.CharField(max_length=128, unique=True)
+    status = models.CharField(choices=st_choices, max_length=16)
+    config = models.JSONField(default=dict, null=True, blank=True)
+
+
+config = {
+    "network": {
+
+    },
+    "users": {
+
+    }
+}
